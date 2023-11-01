05c99e10e19a3ba23f6ff08e58f208c05d87e445
jwillardsunrise <149590029+jwillardsunrise@users.noreply.github.com>
2023-11-01 15:04:42 -0400

    Update ScheduledTransfer.cs - trying to add Regex reference again

--- azstorage-to-s3-main/src/AzStorageTransfer/AzStorageTransfer.FuncApp/ScheduledTransfer.cs
+++ azstorage-to-s3-main/src/AzStorageTransfer/AzStorageTransfer.FuncApp/ScheduledTransfer.cs
@@ -10,7 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
-// using System.Text.RegularExpressions;
+using System.Text.RegularExpressions;
 
 
 namespace AzStorageTransfer.FuncApp
