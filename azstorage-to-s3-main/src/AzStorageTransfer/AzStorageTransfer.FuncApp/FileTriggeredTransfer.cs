05056d2e08c758420f08ac4d8c10bb5a620b5fa9
Prani2004 <149486547+Prani2004@users.noreply.github.com>
2023-11-02 22:33:32 +0530

    FileTriggeredTransfer.cs - added transfer and archive function

--- azstorage-to-s3-main/src/AzStorageTransfer/AzStorageTransfer.FuncApp/FileTriggeredTransfer.cs
+++ azstorage-to-s3-main/src/AzStorageTransfer/AzStorageTransfer.FuncApp/FileTriggeredTransfer.cs
@@ -11,6 +11,7 @@ namespace AzStorageTransfer.FuncApp
     public class FileTriggeredTransfer
     {
         private readonly IAmazonS3 amazonS3;
+        private readonly CloudBlobContainer FileExt;
 
         public FileTriggeredTransfer(IAmazonS3 amazonS3)
         {
@@ -27,13 +28,36 @@ namespace AzStorageTransfer.FuncApp
             log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Properties.Length} Bytes");
 
             using (var ms = new MemoryStream())
+            
             {
+                // Get the URI of the blob
+                var Uri = $"{name}";
+                log.LogInformation($"the item url is: {Uri}");
+
+                // Get the file extension required for transfer
+                var FileExt = $"{Config.FileExt}";
+
+                // Generate Regex
+                var rgxText = @$".*\.{FileExt}";
+                log.LogInformation(rgxText);
+                Regex rgx = new Regex(rgxText);
+
+                if (rgx.IsMatch(Uri))
+                {
+
                 // Download blob content to stream
                 await myBlob.DownloadToStreamAsync(ms);                
                 ms.Seek(0, SeekOrigin.Begin);
                 
                 // Upload stream to S3
                 await this.amazonS3.UploadObjectFromStreamAsync(Config.Aws.BucketName, name, ms, new Dictionary<string, object>());
+
+                }
+                else
+                {
+                    log.LogInformation($"Not going to transfer and archive this url: {Uri}");
+                }
+                
             }
         }
     }
